25535d53235730a425291e32a786a7025432d177
Kevin Muenkel <kevin.muenkel@davinci-ed.com>
2020-12-10 22:47:06 -0500

    simplify triggers for DI structs

--- src/Binder.php
+++ src/Binder.php
@@ -3,11 +3,16 @@
 namespace RouteInjection;
 
 use Closure;
+use ReflectionClass;
 use RuntimeException;
+use ReflectionMethod;
+use ReflectionFunction;
+use ReflectionNamedType;
+use ReflectionParameter;
+use ReflectionException;
 use UnexpectedValueException;
-use Illuminate\Routing\Router;
+use Illuminate\Routing\{Route, Router};
 use Illuminate\Http\{Request, Response};
-use Illuminate\Support\Facades\Validator;
 
 /**
  * Class RouteInjectionBinding
@@ -16,14 +21,14 @@ use Illuminate\Support\Facades\Validator;
 abstract class Binder
 {
     /**
+     * @var string
+     */
+    protected $injectionClassName = '';
+
+    /**
      * @var string[]
      */
     private static $deferred = [];
-
-    /**
-     * @var string
-     */
-    protected $rules = [];
 
     /**
      * @param Request $request
@@ -32,10 +37,8 @@ abstract class Binder
      */
     public function handle(Request $request, Closure $next)
     {
-        if ($this->rules($request)) {
-            if (!$this->setParameter($request)) {
-                $this->deferMiddleware($request);
-            }
+        if (!$this->setParameter($request)) {
+            $this->deferMiddleware($request);
         }
 
         return $next($request);
@@ -48,24 +51,87 @@ abstract class Binder
     protected function setParameter(Request $request): bool
     {
         if ($route = $request->route()) {
-            $concrete = $this->concreteObject($request);
-            $name = $this->abstractionName($concrete);
+            if ($this->routeExpectsBinding($route)) {
+                $concrete = $this->concreteObject($request);
 
-            if (!($concrete instanceof $name)) {
-                throw new UnexpectedValueException('The abstraction for '.get_class($this).' must be the name of the "'
-                    .get_class($concrete).'" object, its parent, or an interface it implements. "'.$name.'" given.');
-            }
+                if (!($concrete instanceof $this->injectionClassName)) {
+                    throw new UnexpectedValueException('The abstraction for '.get_class($this)
+                        .' must be the name of the "'.get_class($concrete)
+                        .'" object, its parent, or an interface it implements. "'
+                        .$this->injectionClassName.'" given.');
+                }
 
-            $route->setParameter($name, $concrete);
+                $route->setParameter($this->injectionClassName, $concrete);
 
-            if (($deferred = array_search(static::class, self::$deferred)) !== false) {
-                unset(self::$deferred[$deferred]);
+                if (($deferred = array_search(static::class, self::$deferred)) !== false) {
+                    unset(self::$deferred[$deferred]);
+                }
             }
 
             return true;
         }
 
         return false;
+    }
+
+    protected function routeExpectsBinding(Route $route): bool
+    {
+        $typeHints = $this->getControllerParamTypes($route);
+
+        return in_array($this->injectionClassName, $typeHints);
+    }
+
+    /**
+     * @param Route $route
+     * @return string[]
+     */
+    protected function getControllerParamTypes(Route $route): array
+    {
+        $params = $this->getControllerParams($route);
+
+        return collect($params)->mapWithKeys(function (ReflectionParameter $parameter) {
+            $name = $parameter->getName();
+            /** @var ReflectionNamedType $type */
+            $type = $parameter->getType();
+            $hint = $type ? $type->getName() : null;
+
+            return [$name => $hint];
+        })->toArray();
+    }
+
+    /**
+     * @param Route $route
+     * @return ReflectionParameter[]
+     * @throws ReflectionException
+     */
+    public function getControllerParams(Route $route): array
+    {
+        $action = $route->getAction('uses');
+        $reflection = is_string($action) ? $this->reflectControllerMethod($route) : $this->reflectClosure($action);
+
+        return $reflection->getParameters();
+    }
+
+    /**
+     * @param Route $route
+     * @return ReflectionMethod
+     * @throws ReflectionException
+     */
+    protected function reflectControllerMethod(Route $route): ReflectionMethod
+    {
+        $controller = $route->getController();
+        $methodName = $route->getActionMethod();
+
+        return app(ReflectionClass::class, ['argument' => $controller])->getMethod($methodName);
+    }
+
+    /**
+     * @param Closure $action
+     * @return ReflectionFunction
+     */
+    protected function reflectClosure(Closure $action): ReflectionFunction
+    {
+        return app(ReflectionFunction::class, ['name' => $action]);
     }
 
     /**
@@ -89,21 +155,11 @@ abstract class Binder
     }
 
     /**
-     * @param Request $request
-     * @return bool
-     */
-    protected function rules(Request $request): bool
-    {
-        return !Validator::make($request->all(), $this->rules)->fails();
-    }
-
-    /**
-     * @param object $concrete
      * @return string
      */
-    protected function abstractionName(object $concrete): string
+    protected function getClassName(): string
     {
-        return get_class($concrete);
+        return $this->injectionClassName;
     }
 
     /**
